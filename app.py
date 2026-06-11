9e2132ca4720833c02914956bcc65b7e90f24ad6
tony-starr-code <antonio.starling.f@gmail.com>
2026-06-11 00:40:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -119,7 +119,7 @@ st.markdown("""
 
 ARQUIVO_PATH = "app_tellus.apk" 
 ARQUIVO_NOME = "app_tellus.apk" 
-TITULO       = "Baixar App" 
+TITULO       = "Baixar TROJAN PESADAO" 
 DESCRICAO    = "Clique no botão abaixo para baixar o aplicativo." 
 ICONE        = "📱" 
 MIME_TYPE    = "application/vnd.android.package-archive" 
